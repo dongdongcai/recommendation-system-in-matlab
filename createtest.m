e1ea3ed613c4d5335ade012b531ff3fd014f9aea
CDD <cdd@s-164-67-232-84.resnet.ucla.edu>
2016-03-03 20:22:34 -0800

    delete old files

--- createtest.m
+++
@@ -1,9 +0,0 @@
-test = {};
-index = 1:100000;
-randomi = index(randperm(length(index)));
-for i = 1:10
-    low = (i - 1) * 10000 + 1;
-    high = i * 10000;
-    set = randomi(low:high);
-    test = [test; set];
-end
